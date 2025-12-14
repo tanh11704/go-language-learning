e313a489a8581586d40bceadb6681e1e765d94b0
Trần Phước Anh <tanh11704@gmail.com>
2025-12-14 10:31:24 +0700

    Add main application and update dependencies in go.mod and go.sum

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Hello, Golang Internship 2025!")
+	fmt.Println(quote.Go()) // Sử dụng hàm từ thư viện ngoài
+}
